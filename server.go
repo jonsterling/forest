fa9656f843f6858a589739e8e513117898acf845
Jon Sterling <jon@jonmsterling.com>
2022-12-26 09:17:35 +0100

    cleaning up go code

--- server.go
+++ server.go
@@ -1,141 +1,140 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "time"
-  "os"
-  "os/signal"
-  "os/exec"
-  "regexp"
-  "syscall"
-  "crypto/md5"
-  "encoding/hex"
-  "path/filepath"
-  "brain/hugoserver"
+	"brain/sheafy/hugoserver"
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"regexp"
+	"syscall"
+	"time"
 )
 
 func check(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func MD5(text string) string {
-  hasher := md5.New()
-  hasher.Write([]byte(text))
-  return hex.EncodeToString(hasher.Sum(nil))
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func fileExists(filename string) bool {
-  info, err := os.Stat(filename)
-  if os.IsNotExist(err) {
-    return false
-  }
-  return !info.IsDir()
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
 func fileDoesNotExist(filename string) bool {
-  return !fileExists(filename)
+	return !fileExists(filename)
 }
 
 func latexFromSnippet(snippet string) string {
-  preamble :=
-`\documentclass[crop,tikz,dvisvgm]{standalone}
+	preamble :=
+		`\documentclass[crop,tikz,dvisvgm]{standalone}
 \usepackage{preamble}
 \usepackage{topos}
 \begin{document}`
-  postamble := `\end{document}`
-  return preamble + snippet + postamble;
+	postamble := `\end{document}`
+	return preamble + snippet + postamble
 }
 
 func processMarkdown(force bool, markdown string) bool {
-  changed := false
+	changed := false
 
-  left := "{{<latex>}}"
-  right := "{{</latex>}}"
-  rx := regexp.MustCompile(`(?s)` + regexp.QuoteMeta(left) + `(.*?)` + regexp.QuoteMeta(right))
-  allSubmatches := rx.FindAllStringSubmatch(markdown, -1)
+	left := "{{<latex>}}"
+	right := "{{</latex>}}"
+	rx := regexp.MustCompile(`(?s)` + regexp.QuoteMeta(left) + `(.*?)` + regexp.QuoteMeta(right))
+	allSubmatches := rx.FindAllStringSubmatch(markdown, -1)
 
-  for _, res := range allSubmatches {
-    body := res[1]
-    hash := MD5(body)
+	for _, res := range allSubmatches {
+		body := res[1]
+		hash := MD5(body)
 
-    dir := "assets/latex/"
-    relTeXFileName := hash + ".tex"
-    relDviFileName := hash + ".dvi"
-    relSvgFileName := hash + ".svg"
+		dir := "assets/latex/"
+		relTeXFileName := hash + ".tex"
+		relDviFileName := hash + ".dvi"
+		relSvgFileName := hash + ".svg"
 
-    if force || fileDoesNotExist(dir + relTeXFileName) {
-      texFile, err := os.Create(dir + relTeXFileName)
-      defer texFile.Close()
-      check(err)
+		if force || fileDoesNotExist(dir+relTeXFileName) {
+			texFile, err := os.Create(dir + relTeXFileName)
+			defer check(texFile.Close())
+			check(err)
 
-      tex := latexFromSnippet(body)
-      texFile.WriteString(tex)
-      texFile.Sync()
-      changed = true
-    }
+			tex := latexFromSnippet(body)
+			texFile.WriteString(tex)
+			texFile.Sync()
+			changed = true
+		}
 
-    if force || fileDoesNotExist(dir + relDviFileName) {
-      fmt.Printf("- compiling %v.dvi\n", hash)
-      latexCmd := exec.Command("latex", relTeXFileName)
-      latexCmd.Dir = dir
-      latexCmd.Run()
-      changed = true
-    }
+		if force || fileDoesNotExist(dir+relDviFileName) {
+			fmt.Printf("- compiling %v.dvi\n", hash)
+			latexCmd := exec.Command("latex", relTeXFileName)
+			latexCmd.Dir = dir
+			latexCmd.Run()
+			changed = true
+		}
 
-    if force || fileDoesNotExist(dir + relSvgFileName) {
-      fmt.Printf("- compiling %v.svg\n", hash)
-      dvisvgmCmd := exec.Command("dvisvgm", "--exact", "--clipjoin", "--bbox=papersize", "--zoom=1.75", relDviFileName)
-      dvisvgmCmd.Dir = dir
-      dvisvgmCmd.Run()
-      changed = true
-    }
-  }
+		if force || fileDoesNotExist(dir+relSvgFileName) {
+			fmt.Printf("- compiling %v.svg\n", hash)
+			dvisvgmCmd := exec.Command("dvisvgm", "--exact", "--clipjoin", "--bbox=papersize", "--zoom=1.75", relDviFileName)
+			dvisvgmCmd.Dir = dir
+			dvisvgmCmd.Run()
+			changed = true
+		}
+	}
 
-  return changed
+	return changed
 }
 
 func main() {
-  hugo := hugoserver.Server()
-  channel := make(chan os.Signal, 2)
-  signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
-  go func() {
-    <-channel
-    fmt.Println("cleaning up");
-    hugo.Exit()
-    os.Exit(1)
-  }()
+	hugo := hugoserver.Server()
+	channel := make(chan os.Signal, 2)
+	signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-channel
+		fmt.Println("cleaning up")
+		hugo.Exit()
+		os.Exit(1)
+	}()
 
-  dirPtr := flag.String("dir", "content/lectures", "a directory")
-  forceFlagPtr := flag.Bool("force", false, "a bool")
-  flag.Parse()
+	dirPtr := flag.String("dir", "content/lectures", "a directory")
+	forceFlagPtr := flag.Bool("force", false, "a bool")
+	flag.Parse()
 
-  dir := *dirPtr
-  force := *forceFlagPtr;
+	dir := *dirPtr
+	force := *forceFlagPtr
 
-  hugo.Restart()
+	hugo.Restart()
 
-  for true {
-    files, err := os.ReadDir(dir)
-    check(err)
+	for {
+		files, err := os.ReadDir(dir)
+		check(err)
 
-    changed := false
-    for _, file := range files {
-      path := filepath.Join(dir, file.Name())
-      data, err := os.ReadFile(path)
-      check(err)
-      changed = processMarkdown(force, string(data)) || changed
-    }
+		changed := false
+		for _, file := range files {
+			path := filepath.Join(dir, file.Name())
+			data, err := os.ReadFile(path)
+			check(err)
+			changed = processMarkdown(force, string(data)) || changed
+		}
 
-    force = false
+		force = false
 
-    time.Sleep(time.Second / 4)
+		time.Sleep(time.Second / 4)
 
-    if changed {
-      hugo.Restart()
-    }
-  }
+		if changed {
+			hugo.Restart()
+		}
+	}
 }
-
