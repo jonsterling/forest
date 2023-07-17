e82c300ff0a54212c8f9ee35851b973f91ccbee7
Jon Sterling <jon@jonmsterling.com>
2023-07-17 15:31:04 +0200

    accidental change

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# opam exec -- forester --dev --base-url https://forest.jonmsterling.com --root jms-0001 trees/
-/Users/jon/Source/ocaml-forester/_build/default/bin/forester/main.exe --dev --root jms-0001 --base-url "https://forest.jonmsterling.com" trees
+opam exec -- forester --dev --base-url https://forest.jonmsterling.com --root jms-0001 trees/
+# /Users/jon/Source/ocaml-forester/_build/default/bin/forester/main.exe --dev --root jms-0001 --base-url "https://forest.jonmsterling.com" trees
