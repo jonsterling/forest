7d3497c966cfa6b537b9cf7a35246e97ecece049
Jon Sterling <jon@jonmsterling.com>
2025-05-25 12:19:51 +0100

    update script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Before using this, run ssh-copy-id site@pallas.mythic-beasts.com
-rsync -avP -e ssh ./output/ site@pallas.mythic-beasts.com:www/www.jonmsterling.com/
+rsync -avP -e --delete ssh ./output/ site@pallas.mythic-beasts.com:www/www.jonmsterling.com/
