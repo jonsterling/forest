c5988d1ac3ba7b35778ff08e785e91f8024b02a9
Jon Sterling <jon@jonmsterling.com>
2025-05-25 16:52:04 +0100

    fixing uploader

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cp htaccess output/.htaccess
 # Before using this, run ssh-copy-id site@pallas.mythic-beasts.com
-rsync -avP -e --delete ssh ./output/ site@pallas.mythic-beasts.com:www/www.jonmsterling.com/
+rsync -avP --delete -e ssh ./output/ site@pallas.mythic-beasts.com:www/www.jonmsterling.com/
